301f745aec6342e1e88c6c1b0034f019659dc5f1
Simone <simone.torrisi98@gmail.com>
2020-05-06 16:51:39 +0200

    Aggiunto esercizio LinuxCLI

---
+++ WebProg/linuxCLI/cli.js
@@ -0,0 +1,128 @@
+$(document).ready(function () {
+
+    let currentIndex = 0;
+    let directory = "";
+    let commandList = {
+        "help": function (parametro) {
+            if (parametro === "")
+                return `
+                    <ul>
+                        <li>help - Visualizza la lista dei comandi;</li>
+                        <li>echo stringa - Stampa la stringa passata come parametro;</li>
+                        <li>cd directory - Cambia directory specificata come parametro;</li>
+                        <li>clear - Pulisce la shell;</li>
+                        <li>somma x y - Calcola la somma x + y;</li>
+                        <li>differenza x y - Calcola la differenza x - y;</li>
+                        <li>prodotto x y - Calcola il prodotto x * y;</li>
+                        <li>divisione x y - Calcola la divisione x / y;</li>
+                        <li>modulo x m - Restituisce il resto della divisione x / m;</li>
+                    </ul>`;
+            return `<p><span class="red-text">Errore</span>: Sintassi errata</p><p>Controlla la sintassi dei comandi digitando il comando help</p>`;
+        },
+        "echo": function (stringa) {
+            if (stringa === "")
+                return `<p><span class="red-text">Errore</span>: Sintassi errata</p><p>Controlla la sintassi dei comandi digitando il comando help</p>`;
+            return `
+                <p>`+ stringa + `</p>
+            `;
+        },
+        "cd": function (stringa) {
+            stringa = stringa.trim().split(' ');
+            if (stringa.length === 1) {
+                if (stringa != "")
+                    directory = "~" + stringa;
+                else
+                    directory = "";
+                return "";
+            }
+            return `<p><span class="red-text">Errore</span>: Sintassi errata</p><p>Controlla la sintassi dei comandi digitando il comando help</p>`;
+        },
+        "clear": function (parametro) {
+            if (parametro === "") {
+                $('#shell').html("");
+                currentIndex = -1;
+                return ``;
+            }
+            return `<p><span class="red-text">Errore</span>: Sintassi errata</p><p>Controlla la sintassi dei comandi digitando il comando help</p>`;
+        },
+        "somma": function (valori) {
+            valori = valori.trim().split(' ');
+            if (valori.length === 2)
+                $.get("operazioni.php", {x:valori[0], y:valori[1], op:"somma"}, function(risultato) {
+                    console.log("risultato: "+risultato);
+                });
+            return `<p><span class="red-text">Errore</span>: Sintassi errata</p><p>Controlla la sintassi dei comandi digitando il comando help</p>`;
+        },
+        "differenza": function (valori) {
+
+        },
+        "prodotto": function (valori) {
+
+        },
+        "divisione": function (valori) {
+
+        },
+        "modulo": function (valori) {
+
+        }
+    }
+
+
+    // FUNZIONI
+    function checkCommand(inputCommand) {
+        let response = "";
+        if (inputCommand !== "") {
+            let command = inputCommand.split(' ', 1)[0].trim();
+            if (command in commandList) {
+                let parametro = "";
+                let indexSpace = inputCommand.indexOf(' ');
+                if (indexSpace > 0) {
+                    parametro = inputCommand.substring(inputCommand.indexOf(' ') + 1).trim();
+                }
+                response = commandList[command](parametro);
+            } else
+                response = `<p><span class="red-text">Errore</span>: Comando sconosciuto</p><p>Controlla la lista dei comandi digitando il comando help</p>`;
+        }
+        return response;
+    }
+
+    function createResponse(response) {
+        $('#shell').append(`
+            <div id="response`+ currentIndex + `" class="response">` + response + `</div>
+        `);
+    }
+
+    function createCommandLine() {
+        $('#shell').append(`
+            <div class="command-line">
+                <div class="user-part"><span class="green-text">simone@simonePC</span>:<span class="blue-text">`+ directory + `</span>~$</div>
+                <div class="text-part"><input type="text" id="input`+ currentIndex + `" class="input-command"></div>
+            </div>
+        `);
+
+        $('#input' + currentIndex).focus();
+
+        $('#input' + currentIndex).keypress(function (event) {
+            if (event.which == 13) {
+                let inputCommand = $('#input' + currentIndex);
+                inputCommand.prop("disabled", true);
+                createResponse(checkCommand(inputCommand.val().trim()));
+                currentIndex++;
+                createCommandLine();
+            }
+        });
+    }
+
+    $('#terminale').draggable({
+        handle: "#shell-header"
+    });
+    $('#terminale').resizable({
+        minWidth: 300,
+        minHeight: 200,
+        handles: "all"
+    });
+    createCommandLine();
+})
+
+
+
---
+++ WebProg/linuxCLI/operazioni.php
@@ -0,0 +1,13 @@
+<?php
+$x = $_GET['x'];
+$y = $_GET['y'];
+$op = $_GET['op'];
+
+echo "funziona";
+/*if(is_float($x) && is_float($y)) {
+
+}*/
+
+
+?>
+
